0c4846ba37be0bbd61c7f73c541f76deb92f4357
jhonitay4376 <nitay06@students.amikom.ac.id>
2022-03-23 18:57:05 +0700

    kirim

---
+++ 2 CLASS & OBJECT/Mahasiswa.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace class_and_objec
+{
+    class Mahasiswa
+    {
+        public string Nim { get; set; }
+        public string Nama { get; set; }
+        public string IPK { get; set; }
+
+        public void registrasi()
+        {
+            Console.WriteLine("Nama : {0}", Nama);
+            Console.WriteLine("Nim  : {0}", Nim);
+            Console.WriteLine("IPK  : {0}", IPK);
+            Console.WriteLine("Telah melakukan registrasi");
+        }
+        public void IsiKrs()
+        {
+            Console.WriteLine("{0} sedang melakukan proses Krs\n ", Nama);
+        }
+    }
+}
---
+++ 2 CLASS & OBJECT/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace class_and_objec
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //membuat objek class
+            Mahasiswa saya = new Mahasiswa();
+            Mahasiswa kamu = new Mahasiswa();
+
+            //input nilai objek
+            saya.Nim = "4376";
+            saya.Nama = "Jono";
+            saya.IPK = "3.9";
+
+            kamu.Nim = "4444";
+            kamu.Nama = " paijo";
+            kamu.IPK = "3.7";
+
+            saya.registrasi();
+            saya.IsiKrs();
+
+            kamu.registrasi();
+            kamu.IsiKrs();
+
+            Console.ReadKey();
+
+        }
+    }
+}
