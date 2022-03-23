a85fe5a06a97ed67e5f5cffd65f6d76483e0cb2e
jhonitay4376 <nitay06@students.amikom.ac.id>
2022-03-23 18:51:57 +0700

    kirim

---
+++ 3 CLASS-TAXI/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TAXI_CLASS
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //objek taxi
+            Taxi member = new Taxi();
+
+            //setting nilai object clas 
+            member.DriverName = "jhonitay";
+            member.OnDuty = true;
+            member.NumPassenger = 10;
+
+            //output informasi
+            member.TaxiInfo();
+            member.PickupPassenger();
+            member.DropOffPassenger();
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ 3 CLASS-TAXI/Taxi.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TAXI_CLASS
+{
+    class Taxi
+    {
+        public string DriverName { get; set; }
+        public bool OnDuty { get; set; }
+        public int NumPassenger { get; set; }
+        public void TaxiInfo()
+        {
+            Console.WriteLine($"Driver Name: {DriverName}");
+            Console.WriteLine($"On Duty: {(OnDuty ? "Yes" : "No")}");
+            Console.WriteLine($"Number of Passenger: {NumPassenger}");
+        }
+        public void PickupPassenger()
+        {
+            Console.WriteLine($"{DriverName} sedang menjemput penumpang");
+        }
+        public void DropOffPassenger()
+        {
+            Console.WriteLine($"{DriverName} selesai mengantar penumpang");
+        }
+    }
+}
