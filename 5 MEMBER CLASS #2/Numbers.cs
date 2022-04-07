d5bc38a37fb5b7cb8400977e99f253f2037ac609
jhonitay4376 <nitay06@students.amikom.ac.id>
2022-04-07 19:45:44 +0700

    07-Apr

---
+++ 5 MEMBER CLASS #2/Numbers.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LatihanOverloadMethod
+{
+    public class Numbers
+    {
+        public int FindMinimum(int number1 , int number2)
+        {
+
+            if (number1 < number2)
+            { return number1; }
+            else
+            { return number2; }
+        }
+
+        public int FindMinimum(int number1 , int number2 , float number3)
+        {
+            if ((number1 < number2) && (number1 < number3))
+            {
+                return number1;
+            }
+            else if ((number2 < number1) && (number2 < number3))
+            {
+                return number2;
+            }
+            else
+            {
+                int x = Convert.ToInt32(number3);
+                return x;
+            }
+        }
+
+        public int FindMaximum(int number1 , int number2)
+        {
+            if (number1 > number2)
+            { return number1; }
+            else
+            { return number2; }
+           
+        }
+
+        public int FindMaximum(int number1 , int number2 , float number3)
+        {
+            if ((number1 > number2)&&(number1 > number3))
+            {
+                return number1;
+            }
+            else if ((number2 > number1) && (number2 > number3))
+            {
+                return number2;
+            }
+            else
+            {
+                int x = Convert.ToInt32(number3);
+                return x;
+            }
+            
+        }
+    }
+}
